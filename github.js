a907e0b73c5abfda56f3f03edd855f3f671b11ce
Luiz Perez <lguilhermepperez@hotmail.com>
2023-12-20 19:30:10 +1000

    Sent code to Git, repos updated

--- github.js
+++ github.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     <div id="introContainer" style="padding: 10px; border: 1px solid #ccc; margin: 10px;">
        
         <div style="flex-grow: 1; margin-left: 10px;">
-            <p>👋 How's it going, folks! 🚀 I'm Luiz Perez, currently navigating the wild seas of coding as a student at MIT xPro's Full Stack Development course in MERN.</p>
+            <p><img src="images/GuiWindow.jpg" alt="Luiz Perez" style="max-width: 100%; height: auto; flex-shrink: 0; width: 150px; margin-bottom: 20px;">👋 How's it going, folks! 🚀 I'm Luiz Perez, currently navigating the wild seas of coding as a student at MIT xPro's Full Stack Development course in MERN.</p>
             <p>Yeah, MERN – it's not a secret code; it stands for MongoDB, Express.js, React.js, and Node.js. I promise I'm not just throwing random letters around.</p>
             <p>When I'm not wrestling with code, you can find me sipping coffee and wondering why my cats gives me that judgmental look every time my code breaks. 😼💻</p>
             <p>I'm on a mission to make programming less mysterious and more relatable because, let's face it, coding is like adulting with a side of error messages.</p>
@@ -15,10 +15,14 @@ document.addEventListener('DOMContentLoaded', function() {
             
             <p>GitHub: <a href="https://github.com/luguipe" target="_blank">luguipe</a></p>
             <p>LinkedIn: <a href="https://www.linkedin.com/in/luizperez/" target="_blank">Luiz's LinkedIn</a></p>
-            <p>Best way to contact me: LinkedIn messages</p>
+            <p>Best way to contact me: LinkedIn messages</p>            
         </div>
-        <img src="GuiWindow.jpg" alt="Luiz Perez" style="max-width: 100%; height: auto; flex-shrink: 0; width: 150px; margin-bottom: 20px;">
-    </div>
+        
+
+        <div>
+            <iframe width="420" height="345" src="https://www.youtube.com/embed/YNWENML4PAA?controls=1" title="Video Introduction"></iframe>
+        </div>
+</div>
    
     `;
 
